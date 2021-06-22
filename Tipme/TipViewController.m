7e9fc3aebd505206ab067f4080afdd2142641ff8
Bruke Wossenseged <brukew@mit.edu>
2021-06-22 16:09:27 -0700

    Built required functionality

---
+++ Tipme/TipViewController.m
@@ -0,0 +1,96 @@
+//
+//  TipViewController.m
+//  Tipme
+//
+//  Created by Bruke Wossenseged on 6/22/21.
+//
+
+#import "TipViewController.h"
+
+@interface TipViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextField *billAmountField;
+@property (weak, nonatomic) IBOutlet UILabel *tipLabel;
+@property (weak, nonatomic) IBOutlet UILabel *totalLabel;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *tipPercentageControl;
+@property (weak, nonatomic) IBOutlet UIView *labelsContainmentView;
+
+@end
+
+@implementation TipViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (IBAction)onTap:(id)sender {
+    NSLog(@"hello");
+    
+    [self.view endEditing:true];
+}
+
+- (IBAction)updateLabel:(id)sender {
+    if (self.billAmountField.text.length == 0){
+        [self hideLabels];
+    }
+    
+    if (self.billAmountField.text.length != 0 && self.labelsContainmentView == 0){
+        [self showLabels];
+    }
+    
+    double tipPercentages[] = {0.15, 0.2, 0.25};
+    double tipPercentage =
+    tipPercentages[self.tipPercentageControl.selectedSegmentIndex];
+    double bill = [self.billAmountField.text
+                   doubleValue];
+    double tip = bill * tipPercentage;
+    double total = bill + tip;
+    self.tipLabel.text = [NSString
+            stringWithFormat: @"$%.2f", tip];
+    self.totalLabel.text = [NSString
+            stringWithFormat: @"$%.2f", total];
+}
+
+- (void)hideLabels {
+    [UIView animateWithDuration:0.5 animations:^{
+        CGRect billFrame = self.billAmountField.frame;
+        billFrame.origin.y += 200;
+        
+        self.billAmountField.frame = billFrame;
+        
+        CGRect labelsFrame = self.labelsContainmentView.frame;
+        labelsFrame.origin.y += 200;
+        self.labelsContainmentView.frame = labelsFrame;
+        
+        self.labelsContainmentView.alpha = 0;
+    }];
+    
+}
+
+- (void)showLabels {
+    [UIView animateWithDuration:0.5 animations:^{
+        CGRect billFrame = self.billAmountField.frame;
+        billFrame.origin.y -= 200;
+        
+        self.billAmountField.frame = billFrame;
+        
+        CGRect labelsFrame = self.labelsContainmentView.frame;
+        labelsFrame.origin.y -= 200;
+        self.labelsContainmentView.frame = labelsFrame;
+        
+        self.labelsContainmentView.alpha = 1;
+    }];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
