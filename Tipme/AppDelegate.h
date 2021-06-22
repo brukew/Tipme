adfca245cff139e1212c1399468c43babcb396a9
Bruke Wossenseged <brukew@mit.edu>
2021-06-22 12:32:49 -0700

    Initial Commit

---
+++ Tipme/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Tipme
+//
+//  Created by Bruke Wossenseged on 6/22/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ TipmeTests/TipmeTests.m
@@ -0,0 +1,36 @@
+//
+//  TipmeTests.m
+//  TipmeTests
+//
+//  Created by Bruke Wossenseged on 6/22/21.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TipmeTests : XCTestCase
+
+@end
+
+@implementation TipmeTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
